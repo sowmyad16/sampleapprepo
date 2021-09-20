1910e89df897b6462fb912b3d14c8ab4e2e69241
sowmyad16 <sowmya.d1609@gmail.com>
2021-09-20 11:29:11 +0530

    show method added!

--- sampleeapp/src/main/java/org/cap/demo/BootClasss.java
+++ sampleeapp/src/main/java/org/cap/demo/BootClasss.java
@@ -4,7 +4,11 @@ public class BootClasss {
 
 	public static void main(String[] args) {
 		System.out.println("Hello world");
+		show();
 
 	}
-
+	
+	public static void show() {
+		System.out.println("Show Method here!");
+	}
 }
